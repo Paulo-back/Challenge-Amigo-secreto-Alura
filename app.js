dddd46519e6dcb5516f322a31525ba0b69f569cb
Paulo-back <rosapaulohenrique95@gmail.com.br>
2025-02-12 19:58:18 -0300

    alterando botoes

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
-
-
 
 
 function adicionarAmigo(){
@@ -11,10 +8,41 @@ function adicionarAmigo(){
 
     }else{
         listaAmigos.push(amigo);
+
         console.log(listaAmigos);
+        for(let i = 0;i<listaAmigos.length;i++){
+            exibirAmigos('#listaAmigos',`${listaAmigos}`);
+            document.getElementById('sortearAmigo').disabled = false;
+
+
+        }
+        limparCampo();
     }
     
 }
-function sortearAmigo(){
+function exibirAmigos() {
+    let listaHTML = document.querySelector('#listaAmigos');
+    listaHTML.innerHTML = "";
 
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = listaAmigos[i];
+        listaHTML.appendChild(item);
+    }
+}
+function exibeSorteado(tag,texto){
+        let campo = document.querySelector(tag);
+        campo.innerHTML = texto;
+     }
+
+function sortearAmigo(){
+        //Colocar um alert & if para senao tiver amigos adicionados errado
+        let amigoSorteado = listaAmigos[Math.floor(Math.random()* listaAmigos.length)];
+    console.log(amigoSorteado);
+    exibeSorteado('#resultado',amigoSorteado);
+     document.getElementById('sortearAmigo').setAttribute('disabled', true);
+}
+function limparCampo(){
+    amigo = document.querySelector('input');
+    amigo.value = '';
 }
